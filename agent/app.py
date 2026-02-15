906de7e5022b8dcaf1204848016c5e144c4641d3
lemonjerome <gabrielcatadmanramos@gmail.com>
2026-02-15 18:55:00 +0800

    feat: rag endpoints

--- agent/app.py
+++ agent/app.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 import requests
 from qdrant_client import QdrantClient
+from rag.ingestion import ingest_document
+from rag.retrieval import retrieve
 
 app = FastAPI()
 
@@ -12,33 +14,41 @@ client = QdrantClient(url="http://qdrant:6333")
 def root():
     return {"status": "notebook-agent running"}
 
-@app.get("/test-llm")
-def test_llm():
-    response = requests.post(
-        OLLAMA_URL,
-        json = {
-            "model": "qwen3:8b",
-            "prompt": "Say hello from Qwen",
-            "stream": False
-        }
-    )
+# @app.get("/test-llm")
+# def test_llm():
+#     response = requests.post(
+#         OLLAMA_URL,
+#         json = {
+#             "model": "qwen3:8b",
+#             "prompt": "Say hello from Qwen",
+#             "stream": False
+#         }
+#     )
 
-    return response.json()
+#     return response.json()
 
-@app.get("/test-stream")
-def test_stream():
-    response = requests.post(
-        OLLAMA_URL,
-        json = {
-            "model": "qwen3:8b",
-            "prompt": "Say hello from Qwen",
-            "stream": True
-        },
-        stream=True
-    )
+# @app.get("/test-stream")
+# def test_stream():
+#     response = requests.post(
+#         OLLAMA_URL,
+#         json = {
+#             "model": "qwen3:8b",
+#             "prompt": "Say hello from Qwen",
+#             "stream": True
+#         },
+#         stream=True
+#     )
 
-    return response.text
+#     return response.text
 
-@app.get("/test-qdrant")
-def test_qdrant():
-    return {"collections": client.get_collections()}
+# @app.get("/test-qdrant")
+# def test_qdrant():
+#     return {"collections": client.get_collections()}
+
+@app.post("/ingest")
+def ingest(data: dict):
+    return ingest_document(data["text"], data["source"])
+
+@app.post("/retrieve")
+def retrieve_endpoint(query: str):
+    return retrieve(query)
